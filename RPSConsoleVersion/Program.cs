5bc2c80e7bb5a8e2ca6cd716d514664dc0a12fa3
Jack McCarthy <jackmcc08@gmail.com>
2021-06-19 00:06:20 +0100

    Test commit

--- RPSConsoleVersion/Program.cs
+++ RPSConsoleVersion/Program.cs
@@ -7,6 +7,7 @@ namespace RPSConsoleVersion
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Test");
         }
     }
 }
